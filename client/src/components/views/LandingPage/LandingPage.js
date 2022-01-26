69d26f9d35a47eaba755053f06d058185c9fda9c
ohminkwon <ohminkwon0720@gmail.com>
2022-01-26 21:25:24 +0900

    Classify page and setup proxy

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import axios from 'axios'
 
 function LandingPage() {
+
+  useEffect(() => {
+    axios.get('/api/hello')
+    .then(response => console.log(response.data))
+  }, [])
+
   return (
     <div>
-      LandingPage
+      <h3>LandingPage 랜딩페이지</h3>
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -27,6 +27,11 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res)=> res.send('Hello World++'))
 
+app.get('/api/hello', (req, res) => {
+
+  res.send("안녕하세요~!")
+})
+
 app.post('/api/users/register', (req, res) => {
 
   //회원 가입 필수 정보들을 client에서 가져와 DB에 넣어준다
